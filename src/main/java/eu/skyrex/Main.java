90e5445af5c234f6ed6ce5f7ece72cd136af7308
ImTheLion <85840208+ImTheLion@users.noreply.github.com>
2024-08-09 12:20:23 +0200

    spawn item frames

--- src/main/java/eu/skyrex/Main.java
+++ src/main/java/eu/skyrex/Main.java
@@ -1,5 +1,6 @@
 package eu.skyrex;
 
+import eu.skyrex.commands.TestCommand;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
@@ -26,8 +27,12 @@ public class Main {
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
             final Player player = event.getPlayer();
             event.setSpawningInstance(instanceContainer);
+            player.setAllowFlying(true);
+            player.setFlying(true);
             player.setRespawnPoint(new Pos(0, 42, 0));
         });
+
+        MinecraftServer.getCommandManager().register(new TestCommand(instanceContainer));
 
         // Start the server on port 25565
         minecraftServer.start("0.0.0.0", 25565);
---
+++ src/main/java/eu/skyrex/commands/TestCommand.java
@@ -0,0 +1,42 @@
+package eu.skyrex.commands;
+
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.command.builder.arguments.number.ArgumentInteger;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.EntityType;
+import net.minestom.server.entity.Player;
+import net.minestom.server.instance.InstanceContainer;
+
+public class TestCommand extends Command {
+    public TestCommand(InstanceContainer manager) {
+        super("test");
+
+        setDefaultExecutor((sender, context) -> {
+            sender.sendMessage("Hello, World!");
+        });
+
+        ArgumentInteger widthArg = ArgumentType.Integer("width");
+        ArgumentInteger heightArg = ArgumentType.Integer("height");
+
+        addSyntax((sender, context) -> {
+            final int width = context.get(widthArg);
+            final int height = context.get(heightArg);
+
+            final Player player = (Player) sender;
+
+            for(int x = player.getPosition().blockX(); x < player.getPosition().blockX() + width; x++) {
+                for(int y = player.getPosition().blockY(); y < player.getPosition().blockY() + height; y++) {
+                    Entity itemFrame = new Entity(EntityType.ITEM_FRAME);
+                    final Pos pos = new Pos(x, y, player.getPosition().blockZ());
+
+                    itemFrame.setInstance(player.getInstance(), pos);
+                }
+            }
+
+            sender.sendMessage("Width: " + width + ", Height: " + height);
+        }, widthArg, heightArg);
+
+    }
+}
