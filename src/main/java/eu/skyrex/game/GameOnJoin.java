ae477946996ebdecbdd487fac1ba47002e9368a5
GameHugo <85233079+GameHugo@users.noreply.github.com>
2024-08-12 22:42:44 +0200

    Add join messages

--- src/main/java/eu/skyrex/game/GameOnJoin.java
+++ src/main/java/eu/skyrex/game/GameOnJoin.java
@@ -1,6 +1,7 @@
 package eu.skyrex.game;
 
 import eu.skyrex.Main;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minestom.server.event.EventListener;
 import net.minestom.server.event.player.PlayerSpawnEvent;
 import net.minestom.server.potion.Potion;
@@ -21,6 +22,15 @@ public class GameOnJoin implements EventListener<PlayerSpawnEvent> {
         event.getPlayer().addEffect(new Potion(PotionEffect.RESISTANCE, (byte) 127, Potion.INFINITE_DURATION));
         Main.getGameManager().addToSidebar(event.getPlayer());
         Main.getGameManager().sendMessageToAllPlayers("<yellow>"+event.getPlayer().getUsername() + " joined the game!");
+        event.getPlayer().sendMessage(MiniMessage.miniMessage().deserialize(
+                "<blue>Welcome to PaintIT! This is a game like skribbl.io, but in Minecraft! You can draw, guess, and have fun!"));
+        if(!Main.getGameManager().isGameStarted()) {
+            event.getPlayer().sendMessage(MiniMessage.miniMessage().deserialize(
+                    "<green>The game has not started yet. You can start it by typing /game start"));
+        } else {
+            event.getPlayer().sendMessage(MiniMessage.miniMessage().deserialize(
+                    "<green>The game has already started, you just automatically join. Good luck!"));
+        }
         return Result.SUCCESS;
     }
 }
