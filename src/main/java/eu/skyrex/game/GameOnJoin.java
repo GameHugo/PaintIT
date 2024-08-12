b5941cf743536c375315f0bcb5959f049b8e448e
GameHugo <85233079+GameHugo@users.noreply.github.com>
2024-08-12 23:13:13 +0200

    Add resourcepack

--- src/main/java/eu/skyrex/game/GameOnJoin.java
+++ src/main/java/eu/skyrex/game/GameOnJoin.java
@@ -1,6 +1,8 @@
 package eu.skyrex.game;
 
 import eu.skyrex.Main;
+import net.kyori.adventure.resource.ResourcePackInfo;
+import net.kyori.adventure.resource.ResourcePackRequest;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minestom.server.event.EventListener;
 import net.minestom.server.event.player.PlayerSpawnEvent;
@@ -8,7 +10,12 @@ import net.minestom.server.potion.Potion;
 import net.minestom.server.potion.PotionEffect;
 import org.jetbrains.annotations.NotNull;
 
+import java.net.URI;
+import java.util.UUID;
+
 public class GameOnJoin implements EventListener<PlayerSpawnEvent> {
+
+    UUID resourcePackId = UUID.randomUUID();
 
     @Override
     public @NotNull Class<PlayerSpawnEvent> eventType() {
@@ -31,6 +38,10 @@ public class GameOnJoin implements EventListener<PlayerSpawnEvent> {
             event.getPlayer().sendMessage(MiniMessage.miniMessage().deserialize(
                     "<green>The game has already started, you just automatically join. Good luck!"));
         }
+
+        URI uri = URI.create("https://download.mc-packs.net/pack/c121153069e3daabd2d369e5b3a6ebdfe04bb3c9.zip");
+        ResourcePackInfo pack = ResourcePackInfo.resourcePackInfo(resourcePackId, uri, "c121153069e3daabd2d369e5b3a6ebdfe04bb3c9");
+        event.getPlayer().sendResourcePacks(ResourcePackRequest.resourcePackRequest().packs(pack));
         return Result.SUCCESS;
     }
 }
