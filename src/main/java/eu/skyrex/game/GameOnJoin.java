310b595140410a8ec8ca670cba573ea970265727
ImTheLion <85840208+ImTheLion@users.noreply.github.com>
2024-08-13 20:26:58 +0200

    invis attempt

--- src/main/java/eu/skyrex/game/GameOnJoin.java
+++ src/main/java/eu/skyrex/game/GameOnJoin.java
@@ -3,11 +3,14 @@ package eu.skyrex.game;
 import eu.skyrex.Main;
 import net.kyori.adventure.resource.ResourcePackInfo;
 import net.kyori.adventure.resource.ResourcePackRequest;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.minestom.server.event.EventListener;
 import net.minestom.server.event.player.PlayerSpawnEvent;
 import net.minestom.server.potion.Potion;
 import net.minestom.server.potion.PotionEffect;
+import net.minestom.server.scoreboard.Team;
+import net.minestom.server.scoreboard.TeamManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.net.URI;
@@ -16,6 +19,10 @@ import java.util.UUID;
 public class GameOnJoin implements EventListener<PlayerSpawnEvent> {
 
     UUID resourcePackId = UUID.randomUUID();
+    private final TeamManager manager = new TeamManager();
+    private final Team team = manager.createBuilder("paintit")
+            .seeInvisiblePlayers()
+            .build();
 
     @Override
     public @NotNull Class<PlayerSpawnEvent> eventType() {
@@ -38,6 +45,9 @@ public class GameOnJoin implements EventListener<PlayerSpawnEvent> {
             event.getPlayer().sendMessage(MiniMessage.miniMessage().deserialize(
                     "<green>The game has already started, you just automatically join. Good luck!"));
         }
+
+        event.getPlayer().addEffect(new Potion(PotionEffect.INVISIBILITY, (byte) 0, Potion.INFINITE_DURATION));
+        team.addMember(((TextComponent)event.getPlayer().getName()).content());
 
         URI uri = URI.create("https://download.mc-packs.net/pack/c121153069e3daabd2d369e5b3a6ebdfe04bb3c9.zip");
         ResourcePackInfo pack = ResourcePackInfo.resourcePackInfo(resourcePackId, uri, "c121153069e3daabd2d369e5b3a6ebdfe04bb3c9");
